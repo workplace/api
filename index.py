5384ee77ceec8fbeb6ceed51d07064665de3cbec
workplace <humbertojunior@gmail.com>
2022-12-21 04:29:39 -0300

    Update index.py

--- index.py
+++ index.py
@@ -12,9 +12,14 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
+@app.get("/hello")
+async def say_hello(name: str):
+    return {"message": f"Hello Michael"}
+
+
 @app.get("/inputs")
 async def getInputs():
     x = ["graduacao","especializacao","mestrado","doutorado","idioma","conhecimento","competencia","certificacao","capacitacao","experiencia"]
     y = [        3.0,             3.0,       1.0,        1.0,     2.0,           4.0,          4.0,           4.0,          3.0,          4.0]
     dip = {"inputs": [{"name": k, "value": v} for k,v in dict(zip(x, y)).items()]}
-    return json.dumps(dip)
+    return (dip)
